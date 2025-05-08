3bfd6a47eeff7218e012d493e93160f52c54db3f
Jonathan Liu <jupyter-jhkliu42@ip-172-31-5-159.us-west-1.compute.internal>
2025-05-08 23:15:42 +0000

    reworking pipeline to save as pq and ignore genome features

---
+++ bin/create_library_dataframe.py
@@ -0,0 +1,196 @@
+### Create a featurized dataframe object from outputs of long read qc pipeline
+
+### PSEUDOCODE
+# - load barcodes and mapped inserts
+# - calculate raw read lengths from fastplong output
+# - qc barcodes and inserts based on mapping quality and barcode length (is_unmapped and mapping_quality)
+# - merge barcodes and inserts on the read ID, use left join on barcodes
+# - append raw read length
+# - aggregate to barcode ID
+# - QC: throw out inserts that have degenerate mapping, then retain median per barcode
+# - save as .pq file
+
+### Data scheme
+# columns - barcode ID, mapped insert start/end (as pd.interval), insert sequence, insert length, empty boolean, median raw read length
+# Throw away is_read1, is_read2, is_proper_pair
+
+import pysam
+import pandas as pd
+from os.path import join
+from tqdm.auto import tqdm
+
+MIN_BARCODE_LENGTH = 42
+MAX_BARCODE_LENGTH = 46
+MAPQ_CUTOFF = 60
+
+def fastq_to_df(fastq_path):
+    """
+    Reads a FASTQ file using pysam and returns a DataFrame
+    with read IDs and sequence lengths. Displays a progress bar.
+
+    Parameters:
+    - fastq_path (str): Path to the FASTQ file (can be gzipped).
+
+    Returns:
+    - pandas.DataFrame: A DataFrame with columns 'read_id' and 'length'.
+    """
+    read_ids = []
+    lengths = []
+
+    with pysam.FastxFile(fastq_path) as fq:
+        # Using tqdm with unknown total
+        with tqdm(desc="Processing reads", unit="read") as pbar:
+            for entry in fq:
+                read_ids.append(entry.name)
+                lengths.append(len(entry.sequence))
+                pbar.update(1)
+
+    df = pd.DataFrame({
+        'read_id': read_ids,
+        'raw_read_length': lengths
+    })
+
+    return df.set_index('read_id')
+
+def calculate_raw_read_lengths(library_path):
+    """
+    Calculate the raw read lengths from a FASTQ file.
+
+    Parameters:
+    - fastq_path (str): Path to the FASTQ file (can be gzipped).
+
+    Returns:
+    - pandas.DataFrame: A DataFrame with columns 'read_id' and 'length'.
+    """
+    return fastq_to_df(join(library_path, 'fastplong.fq'))
+
+def load_bam_to_dataframe(bam_path, max_reads=None):
+    """
+    Load reads from a BAM file into a pandas DataFrame.
+
+    Parameters:
+        bam_path (str): Path to the BAM file.
+        max_reads (int, optional): Max number of reads to load. Useful for large files.
+
+    Returns:
+        pd.DataFrame: DataFrame with read properties.
+    """
+    bamfile = pysam.AlignmentFile(bam_path, "rb")
+    records = []
+
+    for i, read in enumerate(bamfile.fetch(until_eof=True)):
+        if max_reads is not None and i >= max_reads:
+            break
+
+        records.append({
+            'read_id': read.query_name,
+            'reference_name': bamfile.get_reference_name(read.reference_id) if read.reference_id != -1 else None,
+            'reference_start': read.reference_start,
+            'reference_end': read.reference_end,
+            'mapping_quality': read.mapping_quality,
+            'is_unmapped': read.is_unmapped,
+            'is_reverse': read.is_reverse,
+            'insert_sequence': read.query_sequence
+        })
+
+    bamfile.close()
+    return pd.DataFrame(records)
+
+def load_data(library_path):
+    '''
+    Load data from long read library qc output
+    '''
+    # Load barcodes
+    barcodes = pd.read_csv(join(library_path,
+                            'barcodes.tsv'), sep='\t', index_col=0, names=['read_id','bc_sequence','blank','bc_length'])
+    barcodes = barcodes.drop(columns=['blank'])
+
+    # Mapped inserts
+    mapped_inserts = load_bam_to_dataframe(join(library_path,
+                                                    'mapped_inserts.bam')).set_index('read_id')
+    
+    # Filter out barcodes that have weird lengths
+    barcodes = barcodes[barcodes.bc_length.between(MIN_BARCODE_LENGTH, MAX_BARCODE_LENGTH)]
+    
+    # Drop unmapped inserts or inserts with poor mapping quality
+    mapped_inserts = mapped_inserts[mapped_inserts['is_unmapped'] == False].drop(columns=['is_unmapped'])
+    mapped_inserts = mapped_inserts[mapped_inserts['mapping_quality'] >= MAPQ_CUTOFF]
+    
+    return barcodes, mapped_inserts
+
+def merge_and_qc_data(barcodes, mapped_inserts, raw_read_lengths):
+    '''
+    Merge barcodes and inserts dataframes and perform QC
+    '''
+
+    # Merge reads to get data that contains both barcodes and inserts (using barcodes as master)
+    merge = pd.merge(
+        barcodes, mapped_inserts, left_index=True, right_index=True, how='left')
+    
+    # Add raw read lengths
+    merge = pd.merge(
+        merge, raw_read_lengths, left_index=True, right_index=True, how='left')
+
+    # Calculate mapped insert length
+    merge['insert_length'] = [len(x) if type(x) is str else None for x in merge['insert_sequence']]
+
+    # Create column label for empty insert sequences
+    empty_barcode_reads = list(set(merge.index.unique()) - set(mapped_inserts.index.unique()))
+    merge['empty_insert'] = False
+    merge.loc[empty_barcode_reads,'empty_insert'] = True
+
+    return merge
+
+def aggregate_data(merge):
+    '''
+    Aggregate data to barcode level. For barcodes with multiple reads, use the modal insert. If degenerate,
+    use the longest insert.
+    '''
+    tqdm.pandas()
+    # Calculate reads per each barcode
+    reads_per_barcode = merge.bc_sequence.value_counts()
+
+    # Construct initial aggregate dataframe with barcodes that have single reads (or empty barcodes)
+    single_read_barcodes = reads_per_barcode[reads_per_barcode == 1].index
+    df_agg = merge[merge.bc_sequence.isin(single_read_barcodes)].copy()
+    df_agg = df_agg.reset_index(drop=True) # Drop read ID column
+    df_agg['n_reads'] = 1
+
+    # For barcodes with multiple reads, check for a modal insert based on length
+    multi_read_barcodes = reads_per_barcode[reads_per_barcode > 1].index
+    merge_multi_reads = merge[merge.bc_sequence.isin(multi_read_barcodes)]
+    
+    def resolve_group(group):
+        lengths = group['insert_length']
+        mode_lengths = lengths.mode()
+
+        if len(mode_lengths) == 1:
+            mode_len = mode_lengths.iloc[0]
+            modal_subset = group[group['insert_length'] == mode_len]
+            # Aggregate modal insert reads
+            return modal_subset.groupby('bc_sequence').agg({
+                'bc_length': 'first',
+                'reference_name': 'first',
+                'reference_start': 'median',
+                'reference_end': 'median',
+                'mapping_quality': 'median',
+                'is_reverse': 'first',
+                'insert_sequence': 'first',
+                'raw_read_length': 'median',
+                'empty_insert': 'first',
+                'insert_length': 'median'
+            }).assign(n_reads = len(modal_subset))
+        else:
+            # Degenerate: use the longest insert
+            max_len = lengths.max()
+            return group[group['insert_length'] == max_len].head(1).set_index('bc_sequence').assign(n_reads = 1)
+        
+    # Group by barcode and apply resolution logic
+    resolved_multi = merge_multi_reads.groupby('bc_sequence', group_keys=False).progress_apply(resolve_group)
+
+    # Reset index and combine
+    resolved_multi = resolved_multi.reset_index()
+    df_agg = pd.concat([df_agg, resolved_multi])
+
+    return df_agg
+
--- bin/pipeline.py
+++ bin/pipeline.py
@@ -1,46 +1,42 @@
 import argparse
-from create_featurized_object import (load_data, 
+from os.path import join
+from create_library_dataframe import (load_data, 
     calculate_raw_read_lengths,
-    merge_and_qc_data, 
-    create_features_and_metadata,
-    create_anndata_object,
-    export_anndata_object
+    merge_and_qc_data,
+    aggregate_data
 )
 
 if __name__ == "__main__":
     # Get library path and genome identifier with argparse
     parser = argparse.ArgumentParser(description="Pipeline for creating a featurized library object.")
-    parser.add_argument("--library_path", type=str, required=True, help="Path to the library.")
-    parser.add_argument("--genome", type=str, required=True, help="Genome identifier.")
+    parser.add_argument("--library_path", type=str, required=True, help="Path to the library files.")
     args = parser.parse_args()
 
     library_path = args.library_path
-    genome = args.genome
 
     # Load data
-    barcodes, mapped_inserts, inserts_only = load_data(library_path, genome)
-
+    print('Loading data...')
+    barcodes, mapped_inserts, = load_data(library_path)
     print('Data loaded.')
 
     # Calculate raw read lengths
+    print('Calculating raw read lengths...')
     raw_read_lengths = calculate_raw_read_lengths(library_path)
+    print('Raw read lengths calculated.')
     
     # Merge and QC data
-    merge, empty_reads = merge_and_qc_data(barcodes, inserts_only, raw_read_lengths)
+    print('Merging and QCing data...')
+    merge = merge_and_qc_data(barcodes, mapped_inserts, raw_read_lengths)
+    print('Data merged and QCed.')
 
-    # Create features and metadata
-    features_full, features_partial, bc_meta_avg, features_meta, empty_barcodes = create_features_and_metadata(
-        merge,
-        mapped_inserts,
-        empty_reads)
+    # Aggregate data
+    print('Aggregating data...')
+    merge = aggregate_data(merge)
+    print('Data aggregated.')
 
-    # Create AnnData object
-    obj = create_anndata_object(features_full, features_partial, bc_meta_avg, features_meta, empty_barcodes)
-
-    # Export AnnData object
+    # Export dataframe object
     out_path = library_path
-    filename = 'featurized_object.h5ad'
-
-    export_anndata_object(obj, out_path, filename)
-
+    filename = 'library_dataframe.parquet'
+    
+    merge.to_parquet(join(out_path, filename), index=False)
     print('Object exported.')
